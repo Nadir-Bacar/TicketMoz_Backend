cb36988fcbd02e1351c4fa10e55d5ab835bcd5e4
Nadir-Bacar <nadir.bacar@pavulla.com>
2025-07-21 08:52:04 +0200

    com

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: true, // Your frontend URL
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    origin: [
+      'https://ticket-moz-seven.vercel.app', // Your frontend URL
+      'http://localhost:3000', // For local development
+    ],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
   await app.listen(process.env.PORT ?? 4000);
